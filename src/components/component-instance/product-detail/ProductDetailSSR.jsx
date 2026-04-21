f63cdf0608f75feaec63041602c9c2f05057a82b
lin <yihsuehlin@accoladeglobal.com>
2026-04-21 11:21:07 +0800

    update product ssr

--- src/components/component-instance/product-detail/ProductDetailSSR.jsx
+++ src/components/component-instance/product-detail/ProductDetailSSR.jsx
@@ -2,25 +2,34 @@
 import PropTypes from "prop-types";
 import React, { Fragment, useState, useEffect } from "react";
 
+import {customer_retrieve_product} from "@/api/product"
 import ProductDetailClient from "./ProductDetailClient";
-const ProductDetailSSR = ({  
+const ProductDetailSSR = ({
 
-    element, 
+    element,
     elementProps,
     mode,
     actions,
     object,
     ...props
 }) => {
-    
+
+    const [product, setProduct] = useState(object)
+
+    //因為商品有時效性 特價期間 庫存 ...等等 hydrate完需要再fetch一次更新
+    useEffect(() => {
+        customer_retrieve_product(product?.uuid).then(res=>{
+            setProduct(res.data)
+        })
+    }, [])
 
 
 
     return (<Fragment>
         <ProductDetailClient   element={element}
-                elementProps={elementProps} product={object} mode={mode} {...props}/>
+                elementProps={elementProps} product={product} mode={mode} {...props}/>
     </Fragment>)
-    
+
 };
 
 ProductDetailSSR.propTypes = {
